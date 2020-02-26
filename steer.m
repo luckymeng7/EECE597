e46615a37817d8319096eff947f7ca3fd8536db0
Monica Li <luckymeng7@gmail.com>
2020-02-26 04:02:20 -0800

    Delete steer.m

--- steer.m
+++
@@ -1,6 +0,0 @@
-% Move towards the qRand from qNearest for maxStepSize
-% Output: the steered position
-function qNew = steer (nearestNode, qRand, maxStepSize)
-    diff = qRand - nearestNode.position;
-    qNew = nearestNode.position + (diff * maxStepSize / norm(diff));
-end 
