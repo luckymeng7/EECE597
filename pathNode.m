22b65864e8b62f24912b45b43547f0908f6f9978
Monica Li <luckymeng7@gmail.com>
2020-02-26 04:02:56 -0800

    Delete pathNode.m

--- pathNode.m
+++
@@ -1,9 +0,0 @@
-% Class for each of current node
-classdef pathNode
-    properties
-        nodeIndex           % Individual index for each node
-        parentNodeIndex     % Parent node index
-        position            % Coordinates on (x,y)
-        pathToNode          % Contains the index list from 0 to node
-    end 
-end 
