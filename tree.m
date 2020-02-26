0eadd898f2b108fffd3a9c97c5303eea50f38672
Monica Li <luckymeng7@gmail.com>
2020-02-26 04:01:30 -0800

    Delete tree.m

--- tree.m
+++
@@ -1,8 +0,0 @@
-% Class for tree, which contains all the nodes
-classdef tree
-    properties 
-        indexSize           % Total number of the nodes
-        allNodesPosition    % List of all existing nodes' position
-        allNodes            % List of all existing nodes
-    end
-end 
