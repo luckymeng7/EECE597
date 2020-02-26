6f4e389ba81a1ad033d6ab014718fe95638a9fb0
Monica Li <luckymeng7@gmail.com>
2020-02-26 04:02:01 -0800

    Delete nearest.m

--- nearest.m
+++
@@ -1,6 +0,0 @@
-% Find the nearest Node 
-function nearestNode = nearest(currentTree, qRand)
-    distanceToAllNodes = vecnorm((currentTree.allNodesPosition - qRand)');
-    index = distanceToAllNodes == min(distanceToAllNodes);
-    nearestNode = currentTree.allNodes(index);
-end
