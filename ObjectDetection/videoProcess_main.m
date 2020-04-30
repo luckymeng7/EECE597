2057426ebce054be4687e79331682271dc44187d
Xuemeng Li <luckymeng7@gmail.com>
2020-04-29 23:11:36 -0700

    Address Maryam's comment for report

--- ObjectDetection/videoProcess_main.m
+++ ObjectDetection/videoProcess_main.m
@@ -1,5 +1,5 @@
 clear ;clc
 % Call funtions for object detection
-objectDetect('C:\Users\Monica\Downloads\Videos\20200324_182551.bag', 500, 1500, 'topview1.avi') 
-objectDetect('C:\Users\Monica\Downloads\Videos\20200324_182551.bag', 500, 1500, 'topview2.avi') 
-objectDetect('C:\Users\Monica\Downloads\Videos\20200324_182551.bag', 500, 1500, 'topview3.avi') 
+objectDetect('..\Videos\20200324_182551.bag', 500, 1500, 'topview1.avi') 
+objectDetect('..\Videos\20200324_182551.bag', 500, 1500, 'topview2.avi') 
+objectDetect('..\Videos\20200324_182551.bag', 500, 1500, 'topview3.avi') 
